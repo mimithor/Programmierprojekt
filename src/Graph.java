1055f9fe4862319f9966e3fed4154c7635686514
NicoStrohbachStudium <k4r106r4ph13>
2021-11-17 18:59:12 +0100

    Version 0.0

---
+++ src/Graph.java
@@ -0,0 +1,24 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class Graph {
+
+    //graph has 25 115 477 nodes and 50 790 030 edges
+    Node[] graph = new Node[25115477];
+
+    public class Node{
+
+        //int index;
+        //Tuple(double xCoordinate, double yCoordinate) position;
+        //List<Tuple(Node destination, double weight)> outgoingEdges = new LinkedList<Tuple(Node destination, double weight)>();
+
+    }
+
+ /*   public class Edge{
+
+        Node destination;
+        double weight;
+
+    }*/
+
+}
